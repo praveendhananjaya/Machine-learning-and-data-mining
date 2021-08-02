9e047d2af6b8071b1d6a7e4449cbb30811f6147c
praveen <praveenbhananjaya@gmail.com>
2021-08-03 01:31:25 +0530

    ML model

--- ILPD/ML_Model.py
+++ ILPD/ML_Model.py
@@ -1,9 +1,23 @@
+import load as dp
+from balance.balance import UpSampling, DownSampling
+import pandas as pd
+
+model = []
+
 class ML:
+    def __init__(self):
+        dataset = dp.Load()
+
+    def sampling(self) -> dp:
+        pass
+
+    def training(self, dp : pd) -> model:
+        pass
+
+    def evaluation(self, model: model) :
+        pass
+
+
+class logisticRegression(ML):
     def sampling(self):
-        pass
 
-    def training(self):
-        pass
-
-    def evaluation(self):
-        pass
